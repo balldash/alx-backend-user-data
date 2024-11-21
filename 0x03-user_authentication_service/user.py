3aa5d9926211a607c0c650f37cce3095dd5560bd
Bislon Zulu <bislonzulu1@outlook.com>
2024-11-21 18:34:18 +0200

    task 0.1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,7 +15,7 @@ class User(Base):
     A class representing a user in the database.
     """
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
